51f7686f5cebf79cf209747f22cfd6eaac5fdfd3
mdzubaidul <freelancerjami@gmail.co>
2022-05-01 05:17:36 +0600

    database conntect

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1wbmx.mongodb.net/mongodb-review-electron-shop?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
