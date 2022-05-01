1230a42553e41e1db728510b6114c501dfce3344
mdzubaidul <freelancerjami@gmail.co>
2022-05-01 06:10:11 +0600

    change

--- api/web.js
+++ api/web.js
@@ -42,7 +42,7 @@ router.route("/product/:id").delete(deleteProduct);
 
 // login
 router.post("/login", userLogin);
-router.get("/verifyUser", apiMiddleware, verifyUser);
+router.get("/verifyUser", verifyUser);
 // category
 
 router.get("/categoryCount", categoryCount);
--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ const { MongoClient } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1wbmx.mongodb.net/mongodb-review-electron-shop?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  useUnifiedTopology: true,
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 let dbConnection;
